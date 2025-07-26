39bac9b2728c731b3e146c697becc4c40f7853d9
jefferson suaza <jesuazav1995@gmail.com>
2025-07-26 09:26:54 -0500

    clase cafe realizada

--- src/cafe.java
+++ src/cafe.java
@@ -1,3 +1,34 @@
 public class cafe {
+String nombre;
+String region;
+double precioPorKilo;
+int cantidadEnKilos;
+
+public cafe( String nombre, String region, double precioPorKilo, int cantidadEnKilos) {
+    this.nombre=nombre;
+    this.region=region;
+    this.precioPorKilo=precioPorKilo;
+    this.cantidadEnKilos=cantidadEnKilos;
+
+}
+
+public void mostrarInformacion() {
+    System.out.println("Nombre: " + nombre);
+    System.out.println("Región: " + region);
+    System.out.println("Precio por kilo: " + precioPorKilo);
+    System.out.println("Cantidad en kilos: " + cantidadEnKilos);
+
+
+}
+
+public double calcularPrecioTotal() {
+    return precioPorKilo * cantidadEnKilos;
+
+}
+
+public void actualizarCantidad(int nuevaCantidad) {
+    this.cantidadEnKilos = nuevaCantidad;
+
+}
 
 }
